457c01af5a074204e3dc49c3b0f11263523cc131
Antoine Cotten <hello@acotten.com>
2019-08-03 01:10:53 +0200

    Remove printing of JAVA_HOME

--- elasticsearch/bin/init_sg.sh
+++ elasticsearch/bin/init_sg.sh
@@ -3,9 +3,6 @@
 JAVA_HOME=$(dirname $BASH_SOURCE)/../jdk
 export JAVA_HOME
 
-dirname $BASH_SOURCE
-echo $JAVA_HOME
-
 plugins/search-guard-7/tools/sgadmin.sh \
 	-cd config/sg/ \
 	-ts config/sg/truststore.jks \
