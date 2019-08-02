553ba817ecb2a8c26e7e2705b570999cf164b3d0
Antoine Cotten <hello@acotten.com>
2019-08-03 00:37:42 +0200

    Troubleshoot Java installation

--- elasticsearch/bin/init_sg.sh
+++ elasticsearch/bin/init_sg.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 
+JAVA_HOME=$(dirname $BASH_SOURCE)/../jdk
+export JAVA_HOME
+
+dirname $BASH_SOURCE
 echo $JAVA_HOME
-which java
 
 plugins/search-guard-7/tools/sgadmin.sh \
 	-cd config/sg/ \
